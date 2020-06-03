d61aa63685fa7f5a05e7fc97d40c7e42d7758a77
Jonas <jonasbfranco@hotmai.com>
2020-06-02 23:06:33 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,24 +1,38 @@
 # jonas_vue
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Comandos no Linux
+
+- sudo yarn global add @vue/cli
+- sudonpm install -g vue-cli
+- vue --version
+- vue -V
+- vue create nome_do_seu_app
+- yarn serve
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container-fluid">
+    <h1>{{ titulo }}</h1>
+
+    <div
+      class="alert alert-warning"
+      role="alert"
+      v-show="usuarios.length === 0"
+    >Não existem registros de usuários !</div>
+
+    <table class="table table-striped" v-show="usuarios.length > 0">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(usuario, index) in usuarios" v-bind:key="usuario.id">
+          <td>{{ usuario.id }}</td>
+          <td>{{ usuario.nome }}</td>
+          <td>
+            <button class="btn btn-danger" v-on:click="deletar(index)">Remover</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="form-group">
+      <p>
+        <input
+          placeholder="Digite um codigo"
+          type="text"
+          v-model="id"
+          name="idUsuario"
+          class="form-control"
+        />
+      </p>
+      <p>
+        <input
+          placeholder="Digite um nome"
+          type="text"
+          v-model="nome"
+          name="nomeUsuario"
+          class="form-control"
+        />
+      </p>
+      <button class="btn btn-primary" v-on:click="adicionar()">Adicionar</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      titulo: "Teste VueJS Jonas",
+      usuarios: [
+        { id: 1, nome: "Joao" },
+        { id: 2, nome: "Luiz" },
+        { id: 3, nome: "Pedro" },
+        { id: 4, nome: "Maria" }
+      ],
+      id: "",
+      nome: ""
+    };
+  },
+  methods: {
+    deletar(parametro) {
+      this.usuarios.splice(parametro, 1);
+    },
+    adicionar() {
+      this.usuarios.push({
+        id: this.id,
+        nome: this.nome
+      });
+
+      this.id = "";
+      this.nome = "";
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
